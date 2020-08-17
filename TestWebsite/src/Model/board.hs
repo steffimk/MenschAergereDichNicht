41079ae7a23275bfef13f852305e355c6cb91b59
Steffi <stefanie.m.kloss@t-online.de>
2020-08-17 11:57:37 +0200

    Fix

--- TestWebsite/src/Model/board.hs
+++ TestWebsite/src/Model/board.hs
@@ -40,7 +40,18 @@ let board = [ (intToField x) | x <- [0..55] ]
 
 let status = BoardStatus{ board = board, homeFields = homeFields }
 
-2
+intToField :: Int -> Field
+intToField x
+  | x == 0          = Field First (Just Yellow) Nothing
+  | x == 14         = Field First (Just Green) Nothing
+  | x == 28         = Field First (Just Blue) Nothing
+  | x == 42         = Field First (Just Red) Nothing
+  | elem x [10..13] = Field Home (Just Green) Nothing
+  | elem x [24..27] = Field Home (Just Blue) Nothing
+  | elem x [38..41] = Field Home (Just Red) Nothing
+  | elem x [52..55] = Field Home (Just Yellow) Nothing
+  | otherwise       = Field Standard Nothing Nothing
+
 initStart :: Color -> [Field]
 initStart color = [Field Start (Just color) (Just (Figure 0 color)), Field Start (Just color) (Just (Figure 1 color)),
                   Field Start (Just color) (Just (Figure 2 color)), Field Start (Just color) (Just (Figure 3 color))]
