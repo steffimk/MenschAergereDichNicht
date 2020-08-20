9171697b2838ad9c845dfc85bc0c2fb54bf5cd4b
Florian Klement <florian.klement@campus.lmu.de>
2020-08-20 11:29:52 +0200

    [ADD] user token

--- TestWebsite/src/Handler/Lobby.hs
+++ TestWebsite/src/Handler/Lobby.hs
@@ -3,17 +3,28 @@ module Handler.Lobby where
 
 import Import
 import Data.Aeson
+import Prelude hiding ((++))
 
-newtype Lobby = Lobby Text
+type CSRF_Token = Text
+data Lobby = Lobby { lobbyname :: Text, player_token :: CSRF_Token } deriving Show
 
 instance ToJSON Lobby where
-    toJSON (Lobby t) = object ["lobbyname" .= t]
+    toJSON (Lobby n p_t) = object ["lobbyname" .= n, "player_token" .= p_t]
 instance FromJSON Lobby where
-    parseJSON = withObject "Lobby" $ \o -> Lobby <$> o .: "lobbyname"
+    parseJSON = withObject "Lobby" $ \o -> Lobby <$> o .: "lobbyname" <*> o.: "player_token"
+
+data OpenLobbies = OpenLobbies { open_lobbies :: [Lobby] } deriving Show
+openLobbies :: OpenLobbies
+openLobbies = OpenLobbies []
+
+addLobby :: Lobby -> OpenLobbies
+-- addLobby lobby = OpenLobbies ((open_lobbies openLobbies) ++ [lobby])
+addLobby lobby = openLobbies { open_lobbies = (open_lobbies openLobbies) ++ [lobby] }
 
 postLobbyCreationR :: Handler Value
 postLobbyCreationR = do
     -- requireCheckJsonBody will parse the request body into the appropriate type, or return a 400 status code if the request JSON is invalid.
     lobby <- requireCheckJsonBody :: Handler Lobby
+    -- addLobby lobby
 
     returnJson lobby
