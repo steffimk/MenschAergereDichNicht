51011c8f59a600f05d67053e35f892b1eeafd5d0
Steffi <stefanie.m.kloss@t-online.de>
2020-08-25 02:09:01 +0200

    Refactoring of board and actions

--- TestWebsite/src/Controller/actions.hs
+++ TestWebsite/src/Controller/actions.hs
@@ -2,31 +2,42 @@ module Actions where
 
 import Prelude
 
-rollTheDice :: Int
-rollTheDice = 3 -- TODO
+rollTheDice RandomGen g => Int -> g -> [Word]
+rollTheDice n = take n . unfoldr (Just . uniformR (1, 6))
 
 moveFigure :: Figure -> Int -> BoardState -> BoardState
 moveFigure figure count boardState
   | !(isValidAction figure count boardState) = boardState
   | otherwise = do
-      let homeOffset = getHomeOffsetOfColor (turn boardState)
-      let currentFieldNumber = fieldNumber (currentField figure) 
+      let currentFieldNumber = fieldNumber (currentField figure)
       case (fieldType (currentField figure)) of
-        Standard -> newBoardState figure (Figure (number figure) (color Figure) (Field Standard (currentFieldNumber+count))) boardState -- TODO: check if entering any Home Fields, own Home Fields or modulo in general
-        Start -> newBoardState figure (Figure (number figure) (color Figure) (Field First (getBoardOffsetOfColor (turn boardState)))) boardState
-        Home -> newBoardState figure (Figure (number figure) (color Figure) (Field Home (currentField+count))) boardState -- TODO: if valid, then just add count 
-        First -> newBoardState figure (Figure (number figure) (color Figure) (Field Standard (currentFieldNumber+count))) boardState
+        Standard x -> newBoardState figure (Figure (number figure) (color Figure) (getNewField (color figure) currentFieldNumber count)) boardState -- TODO: check if entering any Home Fields, own Home Fields or modulo in general
+        Start -> newBoardState figure (Figure (number figure) (color Figure) (First (getBoardOffsetOfColor (turn boardState)))) boardState
+        Home x -> newBoardState figure (Figure (number figure) (color Figure) (Home (currentField+count))) boardState -- TODO: if valid, then just add count 
+        First x -> newBoardState figure (Figure (number figure) (color Figure) (Standard (currentFieldNumber+count))) boardState
+        -- TODO: Prüfen ob Figur rausgeworfen wurde
 
 isValidAction :: Figure -> Int -> BoardState -> Bool
-isValidAction figure count boardState
-  | (turn boardState) != (color figure)                                       = False
-  | hasToMoveFromFirst boardState && fieldType (currentField figure) != First = False
-  -- TDOO: | count == 6: Noch Figur in Start und keine in Home 
+isValidAction (Figure color currentField) count (BoardState figures turn)
+-- Wenn Farbe ziehen will, die nicht dran ist
+  | color != turn   = False
+-- Wenn First von eigener Figur belegt und noch mind 1 Figur im Start: Muss Figur von First wegbewegen
+  | elem (Figure color (First (getBoardOffsetOfColor color))) figures && (isStartNotEmpty color)
+    = do case currentField of 
+                First x   -> True
+                otherwise -> False
+-- Wenn sechs gewürfelt und noch mind 1 Figur im Start: Figur aufs Spielbrett bewegen
+  | count == 6 && (isStartNotEmpty color) = do
+    if currentField == Start
+      then True
+      else False
+  | if (getNewField color ) -- TODOOOO
   -- TODO: | Figur eigener Farbe im Weg, geht nicht weil Home nicht richtig getroffen wird
 
-hasToMoveFromFirst :: BoardState -> Bool
-hasToMoveFromFirst boardState = do
-  let fieldTypesOfFigures = map (\x -> fieldType (currentField x)) (getFiguresOfColor (figures boardState) (turn boardState))
-  if elem First fieldTypesOfFigures && elem Start fieldTypesOfFigures
-    then True
-    else False
+isStartNotEmpty :: Color -> BoardState -> Bool
+isStartNotEmpty color (BoardState figures _) = elem (Figure color Start) figures
+
+getNewFieldNumber :: Color -> Int -> Int -> Int
+getNewFieldNumber color fieldNumber count = do
+  let homeOffset = getHomeOffsetOfColor color
+  if (fieldNumber + count) >= homeOffset && (fieldNumber + count) < (homeOffset + 4) -- TODO
--- TestWebsite/src/Model/board.hs
+++ TestWebsite/src/Model/board.hs
@@ -4,52 +4,24 @@ import Prelude
 
 data Color = Yellow | Green | Blue | Red deriving (Eq, Enum, Show)
 
-data FieldType = Standard | Start | Home | First deriving (Eq, Enum, Show)
-
 data Figure = Figure {
-                number :: Int,
                 color  :: Color,
                 currentField :: Field
               } deriving Show
 
-data Field = Field {
-              fieldType   :: FieldType,
-              fieldNumber :: Int
-            } deriving Show
-
-data AllFigures = AllFigures {
-              yellowFigures :: [Figure],
-              greenFigures  :: [Figure],
-              blueFigures   :: [Figure],
-              redFigures    :: [Figure]
-            } deriving Show
+data Field = Standard Int | Start | Home Int | First Int deriving (Eq, Show)
 
 data BoardState = BoardState {
-        figures     :: AllFigures,
-        turn        :: Color
+              figures :: [Figure],
+              turn        :: Color
       } deriving Show
-
--- data StartFields = StartFields {
---               yellowHome :: [Field],
---               greenHome  :: [Field],
---               blueHome   :: [Field],
---               redHome    :: [Field]
---             } deriving Show
 
 initNewBoardState :: BoardState
 initNewBoardState = do
-    let figures = AllFigures (initFigures Yellow) (initFigures Green) (initFigures Blue) (initFigures Red)
-    BoardState{ figures = figures , turn = Yellow } -- TODO: Gelb beginnt immer?
+    BoardState ((initFigures Yellow)++(initFigures Green)++(initFigures Blue)++(initFigures Red)) Yellow -- TODO: immer gelb?
 
 initFigures :: Color -> [Figure]
-initFigures color = [Figure x color (Field Start x) | x <- [0..3]]
-
-getFiguresOfColor :: AllFigures -> Color -> [Figure]
-getFiguresOfColor allFigures color
-  | color == Yellow = yellowFigures allFigures
-  | color == Green  = greenFigures allFigures
-  | color == Blue   = blueFigures allFigures
-  | otherwise       = redFigures allFigures
+initFigures color = [Figure color Start | x <- [0..3]]
 
 getBoardOffsetOfColor :: Color -> Int
 getBoardOffsetOfColor color = 
@@ -69,13 +41,13 @@ getHomeOffsetOfColor color =
 
 intToField :: Int -> Field
 intToField x
-  | x == 0          = Field First x
-  | x == 14         = Field First x
-  | x == 28         = Field First x
-  | x == 42         = Field First x
-  | elem x [10..13] = Field Home x
-  | elem x [24..27] = Field Home x
-  | elem x [38..41] = Field Home x
-  | elem x [52..55] = Field Home x
-  | otherwise       = Field Standard x
+  | x == 0          = First x
+  | x == 14         = First x
+  | x == 28         = First x
+  | x == 42         = First x
+  | elem x [10..13] = Home x
+  | elem x [24..27] = Home x
+  | elem x [38..41] = Home x
+  | elem x [52..55] = Home x
+  | otherwise       = Standard x
 
