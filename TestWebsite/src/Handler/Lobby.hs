7d50124aaa11a58c9dbf22aab2171b4250cc2d28
Florian Klement <florian.klement@campus.lmu.de>
2020-08-27 10:35:18 +0200

    [ADD] addLobby monad thingy

--- TestWebsite/src/Handler/Lobby.hs
+++ TestWebsite/src/Handler/Lobby.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Handler.Lobby where
 
-import Import
-import Data.Aeson
-import Prelude hiding ((++))
+import           Control.Monad.State
+import           Data.Aeson
+import           Import
+import           Prelude             hiding ((++))
 
 type CSRF_Token = Text
 data Lobby = Lobby { lobbyname :: Text, player_token :: CSRF_Token } deriving Show
@@ -13,18 +14,44 @@ instance ToJSON Lobby where
 instance FromJSON Lobby where
     parseJSON = withObject "Lobby" $ \o -> Lobby <$> o .: "lobbyname" <*> o.: "player_token"
 
-data OpenLobbies = OpenLobbies { open_lobbies :: [Lobby] } deriving Show
-openLobbies :: OpenLobbies
-openLobbies = OpenLobbies []
+-- data OpenLobbies = OpenLobbies { open_lobbies :: [Lobby] } deriving Show
+-- openLobbies :: OpenLobbies
+-- openLobbies = OpenLobbies []
 
-addLobby :: Lobby -> OpenLobbies
--- addLobby lobby = OpenLobbies ((open_lobbies openLobbies) ++ [lobby])
-addLobby lobby = openLobbies { open_lobbies = (open_lobbies openLobbies) ++ [lobby] }
+data OpenLobbies2 = OpenLobbies2 { open_lobbies :: [Lobby] }
+
+addLobby :: Lobby -> State OpenLobbies2 ()
+addLobby lobby = do
+    currentState <- Control.Monad.State.get
+    let openLobbies = open_lobbies currentState
+    put (currentState { open_lobbies = openLobbies ++ [lobby]})
+    return ()
+
+
+-- openLobbies2 :: [Lobby] -> Lobby -> IO OpenLobbies2
+-- openLobbies2 lobby = do
+--     let new_lobby_list = (open_lobbies openLobbies2) ++ [lobby]
+--     return $ OpenLobbies2 { open_lobbies = new_lobby_list }
+
+-- addLobby :: Lobby -> OpenLobbies2
+-- -- addLobby lobby = OpenLobbies ((open_lobbies openLobbies) ++ [lobby])
+-- addLobby lobby = openLobbies { open_lobbies = (open_lobbies openLobbies) ++ [lobby] }
 
 postLobbyCreationR :: Handler Value
 postLobbyCreationR = do
     -- requireCheckJsonBody will parse the request body into the appropriate type, or return a 400 status code if the request JSON is invalid.
     lobby <- requireCheckJsonBody :: Handler Lobby
-    -- addLobby lobby
+    -- let openLobbies2 = openLobbies { open_lobbies = (open_lobbies openLobbies) ++ [lobby] }
+    -- openLobbies <- lobby
+    -- nuffin <- addLobby lobby
 
     returnJson lobby
+
+-- postLobbyCreationR :: Handler Value
+-- postLobbyCreationR = do
+--     -- requireCheckJsonBody will parse the request body into the appropriate type, or return a 400 status code if the request JSON is invalid.
+--     lobby <- requireCheckJsonBody :: Handler Lobby
+--     -- let openLobbies2 = openLobbies { open_lobbies = (open_lobbies openLobbies) ++ [lobby] }
+--     addLobby lobby
+
+--     returnJson lobby
